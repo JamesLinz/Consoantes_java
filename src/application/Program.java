2196c570ee7846d1cff399ea4dce9259e908dd70
JamesLinz <jlalencar@gmail.com>
2021-11-24 16:35:36 -0300

    Projeto Criado

---
+++ src/application/Program.java
@@ -0,0 +1,43 @@
+/*
+Programa que lê um vetor de 6 caracteres, e diz quantas consoantes foram lidas.
+Imprima as consoantes.
+*/
+
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String[] consoantes = new String[6];
+		int quantidadeConsoantes = 0;
+		
+		int count = 0;
+		do {
+			System.out.println("Letra: ");
+			String letra = sc.next();
+			
+			if ( !(letra.equalsIgnoreCase("a")|
+					letra.equalsIgnoreCase("e")|
+					letra.equalsIgnoreCase("i")|
+					letra.equalsIgnoreCase("o")|
+					letra.equalsIgnoreCase("u")) ) {
+				
+				consoantes[count] = letra;
+				quantidadeConsoantes++;
+			}
+					
+			count++;
+									
+		} while (count < consoantes.length);
+		
+		System.out.println("Consoantes: ");
+		for (String consoante : consoantes) {
+			if (consoante != null)
+				System.out.println(consoante + " ");
+		}
+	}
+}
